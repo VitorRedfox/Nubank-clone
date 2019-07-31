2d34c0d31c438e6b691f6be64f571c2855f20313
VitorRedfox <vitor_pereira.2013@hotmail.com>
2019-07-31 07:44:20 -0300

    feat: menu qr code

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,40 @@
 import React from 'react';
 
-import Header from '~/components/Header';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
+
+import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles';
 
 export default function Main() {
   
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Menu />
+        
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponivel</Title>
+            <Description>R$ 872.148,20</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 20,00 recebida de Vitor Pereira hoje às 06:00hrs 
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
+      <Tabs />
     </Container>
   )
 }
